d71fce844ff0e784be1b880f735d6bde75d66b85
Theodorphus <42559185+Theodorphus@users.noreply.github.com>
2022-04-25 16:11:50 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using VendMachine2;
+
+namespace VendMachine2;
+
+
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+
+
+        var drinkMachine = new DrinkMachine();
+
+
+
+        Console.WriteLine("Please insert one of the amounts: (1, 5, 10, 20, 50, 100, 500, 1000)");
+        drinkMachine.InsertMoney(Convert.ToInt32(Console.ReadLine()));
+
+        while (drinkMachine.checkTotal() == true)
+        {
+
+            drinkMachine.ShowAll();
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
